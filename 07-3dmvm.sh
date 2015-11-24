86558436d42823cdbdd61774d2d08f55d4191923
colmconn <colmconn+github@gmail.com>
2015-11-24 11:19:58 -0800

    Handle group by gender and group by age models and run them in parallel.

--- 07-3dmvm.sh
+++ 07-3dmvm.sh
@@ -62,16 +62,21 @@ fi
 
 function fixDataTable {
     local dataTable="$1"
+    echo "Fixing $dataTable"
 
     lineCount=$( wc -l $dataTable | awk '{print $1}' )
     head -n $( expr $lineCount - 1 ) < $dataTable > ${dataTable}.new
-    tail -1 $dataTable | sed "s/\\\\$//" >> ${dataTable}.new
-    
+    tail -1 $dataTable | sed 's/\\$//' >> ${dataTable}.new
+
     mv -f ${dataTable}.new ${dataTable}
+
 }
 
 cd $GROUP_RESULTS
 grouping="mddAndCtrl"
+
+taskFile="$scriptsDir/3dmvm-taskFile.txt"
+cat /dev/null > $taskFile
 
 for seed in $seeds ; do
 
@@ -82,28 +87,112 @@ for seed in $seeds ; do
 	seedName=${seedName%%+*}
     fi
     
-    dataTableFilename=${GROUP_DATA}/dataTable.${grouping}.${seedName}.txt
-    fixDataTable $dataTableFilename
     if [[ -f $GROUP_RESULTS/mask.grey.$grouping.union.masked+tlrc.HEAD ]] ; then
 	mask=$GROUP_RESULTS/mask.grey.$grouping.union.masked+tlrc.HEAD
     else
 	mask=$MDD_STANDARD/MNI152_T1_3mm_brain_mask.nii.gz
     fi
 
+
+    dataTableFilename=${GROUP_DATA}/dataTable.${grouping}.group.and.gender.${seedName}.txt
+    fixDataTable $dataTableFilename
     timestamp=$( date +%Y%m%d-%H%M%Z ) 
 
-    echo "3dLME -prefix restingstate.mddAndCtrl.$seedName.lme.bucket.$timestamp \
-	  -jobs 8 \
-	  -mask $mask \
-	  -qVars age.in.years \
-	  -model'Group*Gender' \
-	  -ranEff '~1' \
-	  -SS_type 3 \
-          -num_glt  2                                                   \
-          -gltLabel 1 'MDD-NCL' -gltCode  1 'Group : 1*MDD -1*NCL'      \
-          -gltLabel 2 'M-F'     -gltCode  2 'Gender : 1*M  -1*F'      \
+    ## ###############################################################
+    ## NOTE WELL NOTE WELL NOTE WELL NOTE WELL NOTE WELL NOTE WELL
+    ## ###############################################################
+    ## When creating contrasts to investigate interations like in GLTs
+    ## 3 and 4 below, ensure that the constant level (e.g., MDD in GLT
+    ## 3 and NCL in GLT 4) is always on the left. If you do not, the
+    ## graphLmeRois.r script will not correctly graph your results for
+    ## you.
+    ## ###############################################################
+    ## NOTE WELL NOTE WELL NOTE WELL NOTE WELL NOTE WELL NOTE WELL
+    ## ###############################################################
+
+    if [[ 1 == 1 ]] ; then 
+    cmd="3dMVM      -prefix $GROUP_RESULTS/restingstate.mddAndCtrl.group.and.gender.$seedName.mvm.bucket.$timestamp \
+	  -jobs     8 \
+	  -mask     $mask \
+	  -bsVars   'Group*Gender+Full' \
+          -qVars    'Full' \
+	  -SS_type  3 \
+          -num_glt  4 \
+          -gltLabel 1 'MDD-NCL'     -gltCode 1 'Group  : 1*MDD -1*NCL' \
+          -gltLabel 2 'M-F'         -gltCode 2 'Gender : 1*M   -1*F' \
+          -gltLabel 3 'M.MDD-M.NCL' -gltCode 3 'Gender : 1*M Group : 1*MDD -1*NCL' \
+          -gltLabel 4 'F.MDD-F.NCL' -gltCode 4 'Gender : 1*F Group : 1*MDD -1*NCL' \
 	  -dataTable \
 	  @$dataTableFilename"
+    #echo "3dLME command is: $cmd"
+    echo "$cmd" >> $taskFile
+    fi
+    
+    dataTableFilename=${GROUP_DATA}/dataTable.${grouping}.group.and.age.${seedName}.txt
+    fixDataTable $dataTableFilename
+    # timestamp=$( date +%Y%m%d-%H%M%Z ) 
+
+    if [[ 0 == 1 ]] ; then 
+    cmd="3dLME      -prefix $GROUP_RESULTS/restingstate.mddAndCtrl.group.and.age.$seedName.lme.bucket.$timestamp \
+    	  -jobs     8 \
+    	  -mask     $mask \
+    	  -model    'Group*age+Full' \
+    	  -ranEff   '~1' \
+          -qVars    'age,Full' \
+    	  -SS_type  3 \
+          -num_glt  4                                                 \
+          -gltLabel 1 'MDD-NCL'    -gltCode  1 'Group : 1*MDD -1*NCL' \
+          -gltLabel 2 'age'        -gltCode  2 'age :' \
+          -gltLabel 3 'MDD-AgeEff' -gltCode  3 'Group : 1*MDD age :'  \
+          -gltLabel 4 'NCL-AgeEff' -gltCode  4 'Group : 1*NCL age :'  \
+    	  -dataTable \
+    	  @$dataTableFilename"
+    #echo "3dLME command is: $cmd"
+    echo "$cmd" >> $taskFile
+    fi
 
 done
     
+## jobname
+#$ -N rsfcLme
+
+## queue
+#$ -q all.q
+
+## binary?
+#$ -b y
+
+## rerunnable?
+#$ -r y
+
+## merge stdout and stderr?
+#$ -j y
+
+## send no mail
+#$ -m n
+
+## execute from the current working directory
+#$ -cwd
+
+## use a shell to run the command
+#$ -shell yes 
+## set the shell
+#$ -S /bin/bash
+
+## preserve environment
+#$ -V 
+
+nTasks=$( cat $taskFile | wc -l )
+
+sge_command="qsub -N amygdalaRsfcLme -q all.q -j y -m n -V -wd $( pwd ) -t 1-$nTasks" 
+#echo $sge_command
+echo "Queuing job... "
+exec $sge_command <<EOF
+#!/bin/sh
+
+#$ -S /bin/sh
+
+command=\`sed -n -e "\${SGE_TASK_ID}p" $taskFile\`
+
+exec /bin/sh -c "\$command"
+EOF
