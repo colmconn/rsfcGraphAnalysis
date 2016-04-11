501166d51e28ae1a80b7c6edf6a2a1743757fddf
colmconn <colmconn+github@gmail.com>
2016-04-11 12:00:43 -0700

    Added scripts to compute ttests for replication of (our) sgACC and Kaiser seeds analysis

---
+++ 07-ttest.withCovariates.mddAndCtrl.Mid_cing.acc.amygdala.paper.sh
@@ -0,0 +1,56 @@
+cd /data/sanDiego/rsfcGraphAnalysis/data//Group.results.acc.amygdala.paper ; 3dttest++ -mask mask.grey.mddAndCtrl.union.masked+tlrc.HEAD \
+-prefix ttest.mddAndCtrl.Mid_cing.covaried \
+-center NONE \
+-setA Attempter 140_A /data/sanDiego/rsfcGraphAnalysis/data//140_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+164_A /data/sanDiego/rsfcGraphAnalysis/data//164_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+167_A /data/sanDiego/rsfcGraphAnalysis/data//167_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+304_A /data/sanDiego/rsfcGraphAnalysis/data//304_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+320_A /data/sanDiego/rsfcGraphAnalysis/data//320_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+323_A /data/sanDiego/rsfcGraphAnalysis/data//323_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+339_A /data/sanDiego/rsfcGraphAnalysis/data//339_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+343_A /data/sanDiego/rsfcGraphAnalysis/data//343_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+356_A /data/sanDiego/rsfcGraphAnalysis/data//356_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+363_A /data/sanDiego/rsfcGraphAnalysis/data//363_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+390_A /data/sanDiego/rsfcGraphAnalysis/data//390_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+-setB Non-attempter 118_A /data/sanDiego/rsfcGraphAnalysis/data//118_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+120_A /data/sanDiego/rsfcGraphAnalysis/data//120_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+133_A /data/sanDiego/rsfcGraphAnalysis/data//133_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+134_A /data/sanDiego/rsfcGraphAnalysis/data//134_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+136_A /data/sanDiego/rsfcGraphAnalysis/data//136_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+137_A /data/sanDiego/rsfcGraphAnalysis/data//137_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+147_A /data/sanDiego/rsfcGraphAnalysis/data//147_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+149_A /data/sanDiego/rsfcGraphAnalysis/data//149_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+150_A /data/sanDiego/rsfcGraphAnalysis/data//150_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+154_A /data/sanDiego/rsfcGraphAnalysis/data//154_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+158_A /data/sanDiego/rsfcGraphAnalysis/data//158_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+160_A /data/sanDiego/rsfcGraphAnalysis/data//160_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+161_A /data/sanDiego/rsfcGraphAnalysis/data//161_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+169/300_A /data/sanDiego/rsfcGraphAnalysis/data//300_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+301_A /data/sanDiego/rsfcGraphAnalysis/data//301_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+309_A /data/sanDiego/rsfcGraphAnalysis/data//309_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+310_A /data/sanDiego/rsfcGraphAnalysis/data//310_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+313_A /data/sanDiego/rsfcGraphAnalysis/data//313_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+316_A /data/sanDiego/rsfcGraphAnalysis/data//316_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+330_A /data/sanDiego/rsfcGraphAnalysis/data//330_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+331_A /data/sanDiego/rsfcGraphAnalysis/data//331_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+332_A /data/sanDiego/rsfcGraphAnalysis/data//332_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+335_A /data/sanDiego/rsfcGraphAnalysis/data//335_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+336_A /data/sanDiego/rsfcGraphAnalysis/data//336_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+338_A /data/sanDiego/rsfcGraphAnalysis/data//338_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+342_A /data/sanDiego/rsfcGraphAnalysis/data//342_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+344_A /data/sanDiego/rsfcGraphAnalysis/data//344_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+345_A /data/sanDiego/rsfcGraphAnalysis/data//345_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+349_A /data/sanDiego/rsfcGraphAnalysis/data//349_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+351_A /data/sanDiego/rsfcGraphAnalysis/data//351_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+353_A /data/sanDiego/rsfcGraphAnalysis/data//353_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+355_A /data/sanDiego/rsfcGraphAnalysis/data//355_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+359_A /data/sanDiego/rsfcGraphAnalysis/data//359_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+360_A /data/sanDiego/rsfcGraphAnalysis/data//360_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+361_A /data/sanDiego/rsfcGraphAnalysis/data//361_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+364_A /data/sanDiego/rsfcGraphAnalysis/data//364_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+368_A /data/sanDiego/rsfcGraphAnalysis/data//368_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+371_A /data/sanDiego/rsfcGraphAnalysis/data//371_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+373_A /data/sanDiego/rsfcGraphAnalysis/data//373_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+389_A /data/sanDiego/rsfcGraphAnalysis/data//389_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+393_A /data/sanDiego/rsfcGraphAnalysis/data//393_A/rsfc/Mid_cing/Mid_cing.z-score.masked+tlrc.HEAD \
+-covariates /data/sanDiego/rsfcGraphAnalysis/data//Group.data.acc.amygdala.paper/3dttest.covariates.mddAndCtrl.Mid_cing.acc.amygdala.paper.txt
