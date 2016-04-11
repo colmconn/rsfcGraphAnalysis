a9959dd60aa4c17f91acf7a825435237a7e25732
colmconn <colmconn+github@gmail.com>
2016-04-11 11:57:31 -0700

    Run more regressions

--- runRegressionsInParallel.sh
+++ runRegressionsInParallel.sh
@@ -11,9 +11,9 @@ RSFC_ROOT=/data/sanDiego
 
 ##regressionVariables="CGAS BDI.II.Total CDRS.t.score MASC.tscore CDI.Total RADS.Total.tscore"
 
-##regressionVariables="CDRS.t.score"
+regressionVariables="CDRS.t.score"
 
-regressionVariables="CDRS.t.score.rstandard.short"
+## regressionVariables="CDRS.t.score.rstandard.short"
 
 
 
@@ -25,7 +25,7 @@ fi
 	    
 for variable in $regressionVariables ; do
 	
-    cat <<EOF > run/run-${variable}-regression.sh
+    cat <<EOF > run/run-${variable}-booted-regression.sh
 #!/bin/bash
 
 ## jobname
@@ -33,7 +33,7 @@ for variable in $regressionVariables ; do
 
 ## queue
 #$ -q all.q
-
+_weights
 ## binary?
 #$ -b y
 
@@ -69,20 +69,32 @@ export JOBS OMP_NUM_THREADS AFNI_COMPRESSOR AFNI_DECONFLICT
 
 cd $RSFC_ROOT/$taskDir/scripts
 
+####################################################################################################
+### Follow-up regressions
+
 ## ./rsfc.parallel.robust.regression.change.r  -e -c new.mdd.${variable}.diff.change.score.csv -v ${variable}.diff -s juelich_amygdala_seeds_weights.txt
 
-# ./rsfc.parallel.robust.regression.change.r  -c new.mdd.${variable}.scaled.diff.change.score.csv -v ${variable}.scaled.diff -s juelich_amygdala_seeds_weights.txt
+## ./rsfc.parallel.robust.regression.change.r  -c new.mdd.${variable}.scaled.diff.change.score.csv -v ${variable}.scaled.diff -s juelich_amygdala_seeds_weights.txt
 
-./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_left_whole_amygdala_seed.txt
-./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_right_whole_amygdala_seed.txt
+./rsfc.parallel.robust.regression.change.r  -c new.mdd.${variable}.scaled.diff.change.score.csv -v ${variable}.scaled.diff -s left_sgacc_seeds.txt
+./rsfc.parallel.robust.regression.change.r  -c new.mdd.${variable}.scaled.diff.change.score.csv -v ${variable}.scaled.diff -s right_sgacc_seed.txt
+
+####################################################################################################
+### Baseline regression comands
+
+##./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_left_whole_amygdala_seed.txt
+##./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_right_whole_amygdala_seed.txt
+
+./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s left_whole_sgacc_seed.txt
+./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s right_whole_sgacc_seed.txt
 
 EOF
 	chmod +x  run/run-${variable}-booted-regression.sh
 	echo $variable
 	## qsub -pe smp 8 \
 	rm -f ../log/${variable}-booted-regression.log
-	qsub -o ../log/${variable}-booted-regression.log \
-	     run/run-${variable}-booted-regression.sh
+#	qsub -o ../log/${variable}-booted-regression.log \
+#	     run/run-${variable}-booted-regression.sh
 done
 
 
