71382f1ca6bd044d779779fcc6236f4d1ade33a8
colmconn <colmconn+github@gmail.com>
2016-04-11 11:56:27 -0700

    Run more regressions

--- runMultpleRegressionsInParallel.sh
+++ runMultpleRegressionsInParallel.sh
@@ -10,21 +10,43 @@ taskFile=./multipleParallBootedRegressions-TaskFile
 cat /dev/null > $taskFile
 
 #regressionVariables="CDRS.t.score.scaled.diff" ## Predictive regressions
-## regressionVariables="CDRS.t.score" ## Baseline regressions
+regressionVariables="CDRS.t.score" ## Baseline regressions
 # regressionVariables="CDRS.t.score.C"
 
 #regressionVariables="CDRS.t.score.rstandard"
 
 # regressionVariables="BDI.II.Total"
-regressionVariables="MASC.tscore"
+## regressionVariables="MASC.tscore"
 for variable in $regressionVariables ; do
 
     ## echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_left_whole_amygdala_seed.txt"  >> ${taskFile}
     ## echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_right_whole_amygdala_seed.txt" >> ${taskFile}
 
-    echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.timepoint.a.score.csv   -v ${variable}       -s juelich_left_whole_amygdala_seed.txt"  >> ${taskFile}
-    echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.timepoint.a.score.csv   -v ${variable}       -s juelich_right_whole_amygdala_seed.txt" >> ${taskFile}
+##    echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.timepoint.a.score.csv   -v ${variable}       -s juelich_left_whole_amygdala_seed.txt"  >> ${taskFile}
+##    echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.timepoint.a.score.csv   -v ${variable}       -s juelich_right_whole_amygdala_seed.txt" >> ${taskFile}
 
+
+####################################################################################################
+### Follow-up regressions
+
+## ./rsfc.parallel.robust.regression.change.r  -e -c new.mdd.${variable}.diff.change.score.csv -v ${variable}.diff -s juelich_amygdala_seeds_weights.txt >> ${taskFile}
+
+## ./rsfc.parallel.robust.regression.change.r  -c new.mdd.${variable}.scaled.diff.change.score.csv -v ${variable}.scaled.diff -s juelich_amygdala_seeds_weights.txt >> ${taskFile}
+
+echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000  -c new.mdd.${variable}.scaled.diff.change.score.csv -v ${variable}.scaled.diff -s left_whole_sgacc_seed.txt" >> ${taskFile}
+echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000  -c new.mdd.${variable}.scaled.diff.change.score.csv -v ${variable}.scaled.diff -s right_whole_sgacc_seed.txt" >> ${taskFile}
+
+####################################################################################################
+### Baseline regression comands
+
+##./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_left_whole_amygdala_seed.txt >> ${taskFile}
+##./rsfc.parallel.robust.regression.change.r  -b -r 100 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s juelich_right_whole_amygdala_seed.txt >> ${taskFile}
+
+echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s left_whole_sgacc_seed.txt" >> ${taskFile}
+echo "./rsfc.parallel.robust.regression.change.r  -b -r 1000 -c new.mdd.${variable}.scores.csv   -v ${variable}       -s right_whole_sgacc_seed.txt" >> ${taskFile}
+
+
+    
 done
 
 ## jobname
